f35431ac96938be62ad898fa0ed2dc9677189416
George <george@MacBook-Pro-George.local>
2021-09-28 13:43:23 +0700

    add Gauss file and matrix data in main

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+m = numpy.array([[1., 1., 15.], [15., 0., 1.], [4., 15., 1.]])
+v = numpy.array([17., 16., 20.])
 
+res = numpy.linalg.solve(m, v)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(res)
